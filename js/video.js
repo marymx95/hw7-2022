95b8e30280c25c743b6253f7d5af1693e0ec80b4
marymx95 <60026000+marymx95@users.noreply.github.com>
2022-11-08 10:39:41 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,116 @@
+var video;
+var playbutton;
+var pausebutton;
+var slowdown;
+var speedup;
+var skip;
+var mute;
+var slider;
+var volume;
+var vid_speed
+var og;
+var os;
+
+window.onload = function () {
+	video = document.getElementById("player1");
+	loading();
+	playbutton = document.getElementById("play");
+	pausebutton = document.getElementById("pause");
+	slowdown = document.getElementById("slower");
+	speedup = document.getElementById("faster");
+	skip = document.getElementById("skip");
+	mute = document.getElementById("mute");
+	slider = document.getElementById("slider");
+	volume = document.getElementById("volume");
+	os = document.getElementById("vintage");
+	og = document.getElementById("orig");
+
+	og.addEventListener("click", function () {
+		video.className = "video";
+	});
+	os.addEventListener("click", function () {
+		video.className = "oldSchool";
+	});
+
+	slider.addEventListener("change", function () {
+		var volume = document.getElementById("slider").value;
+		document.getElementById("volume").innerHTML = volume + "%";
+		video.volume = volume / 100;
+		console.log(video.volume)
+	});
+	playbutton.addEventListener("click", function () {
+		console.log("Play the video");
+
+		video.play();
+
+
+	});
+	pausebutton.addEventListener("click", function () {
+		console.log("Pause the video");
+		video.pause();
+		var volume = document.getElementById("slider").value;
+		document.getElementById("volume").innerHTML = volume + "%";
+
+	});
+	slowdown.addEventListener("click", function () {
+		vid_speed = video.playbackRate;
+		vid_speed = vid_speed - 0.1;
+		video.playbackRate = vid_speed;
+		console.log("Slowed down video speed to", video.playbackRate);
+
+
+	});
+	speedup.addEventListener("click", function () {
+		vid_speed = video.playbackRate;
+		vid_speed = vid_speed + 0.1;
+		video.playbackRate = vid_speed;
+		console.log("Sped up video speed to", video.playbackRate);
+
+
+	});
+
+	skip.addEventListener("click", function () {
+
+		console.log(video.currentTime)
+
+		var newtime = video.currentTime + 10;
+		if (newtime >= video.duration) {
+			video.currentTime = 0
+		}
+		else {
+			video.currentTime = newtime;
+		}
+		console.log("Skipped ahead in the video to", video.currentTime)
+	});
+
+	mute.addEventListener("click", function () {
+
+		if (video.muted) {
+			video.muted = false;
+			console.log("Video unmuted")
+		}
+		else {
+			video.muted = true;
+			console.log("Video muted")
+		}
+
+
+
+
+	});
+
+}
+function loading() {
+	console.log("Good job opening the window");
+	video.autoPlay = false;
+	video.loop = false;
+
+}
+
+
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
+
